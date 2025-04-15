e2367a912f54b2c6cb688ba44246cb2829bd7015
dsw56021 <mcpremium.karpiu@gmail.com>
2025-04-15 18:41:44 +0200

    newFlags

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" == "--logs" ]; then
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
   if [[ "$2" =~ ^[0-9]+$ ]]; then
     for i in $(seq 1 "$2"); do
       filename="log${i}.txt"
@@ -10,15 +10,15 @@ if [ "$1" == "--logs" ]; then
     done
     echo "Utworzono $2 plików logx.txt."
   else
-    echo "Podano niepoprawny argument liczby plików. Użycie: ./skrypt.sh --logs [liczba]"
+    echo "Podano niepoprawny argument liczby plików. Użycie: ./skrypt.sh --logs [liczba] lub ./skrypt.sh -l [liczba]"
   fi
-elif [ "$1" == "--date" ]; then
+elif [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
   echo "Aktualna data: $(date)"
-elif [ "$1" == "--help" ]; then
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
   echo "Dostępne opcje:"
-  echo "  --logs [liczba]  - Tworzy określoną liczbę plików logx.txt (domyślnie 100)."
-  echo "  --date           - Wyświetla aktualną datę."
-  echo "  --help           - Wyświetla tę wiadomość pomocy."
+  echo "  --logs [liczba], -l [liczba]   - Tworzy określoną liczbę plików logx.txt (domyślnie 100)."
+  echo "  --date, -d                    - Wyświetla aktualną datę."
+  echo "  --help, -h                    - Wyświetla tę wiadomość pomocy."
 else
-  echo "Nieznana opcja. Użyj ./skrypt.sh --help, aby zobaczyć dostępne opcje."
+  echo "Nieznana opcja. Użyj ./skrypt.sh --help lub ./skrypt.sh -h, aby zobaczyć dostępne opcje."
 fi
