16e4b2e523113da42f5dd59a2f7c229e3b6b650e
dsw56021 <mcpremium.karpiu@gmail.com>
2025-04-15 18:22:52 +0200

    --logs

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,15 @@ if [ "$1" == "--date" ]; then
 else
   echo "Użycie: ./skrypt.sh --date"
 fi
+
+if [ "$1" == "--logs" ]; then
+  for i in {1..100}; do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: $filename" > "$filename"
+    echo "Nazwa skryptu: $0" >> "$filename"
+    echo "Data utworzenia: $(date)" >> "$filename"
+  done
+  echo "Utworzono 100 plików logx.txt."
+else
+  echo "Użycie: ./skrypt.sh --logs"
+fi
