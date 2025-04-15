afe9a806b978c569647596a4314953ce9393d5e2
dsw56021 <mcpremium.karpiu@gmail.com>
2025-04-15 18:36:18 +0200

    help arg

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-  echo "Aktualna data: $(date)"
-else
-  echo "Użycie: ./skrypt.sh --date"
-fi
-
 if [ "$1" == "--logs" ]; then
   if [[ "$2" =~ ^[0-9]+$ ]]; then
     for i in $(seq 1 "$2"); do
@@ -18,6 +12,13 @@ if [ "$1" == "--logs" ]; then
   else
     echo "Podano niepoprawny argument liczby plików. Użycie: ./skrypt.sh --logs [liczba]"
   fi
+elif [ "$1" == "--date" ]; then
+  echo "Aktualna data: $(date)"
+elif [ "$1" == "--help" ]; then
+  echo "Dostępne opcje:"
+  echo "  --logs [liczba]  - Tworzy określoną liczbę plików logx.txt (domyślnie 100)."
+  echo "  --date           - Wyświetla aktualną datę."
+  echo "  --help           - Wyświetla tę wiadomość pomocy."
 else
-  echo "Użycie: ./skrypt.sh --logs [liczba]"
+  echo "Nieznana opcja. Użyj ./skrypt.sh --help, aby zobaczyć dostępne opcje."
 fi
