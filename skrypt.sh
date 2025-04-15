f551052a3d2b91412571902ee71e8b2501faa903
dsw56021 <mcpremium.karpiu@gmail.com>
2025-04-15 18:45:20 +0200

    init

--- skrypt.sh
+++ skrypt.sh
@@ -12,6 +12,24 @@ if [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
   else
     echo "Podano niepoprawny argument liczby plików. Użycie: ./skrypt.sh --logs [liczba] lub ./skrypt.sh -l [liczba]"
   fi
+elif [ "$1" == "--init" ]; then
+
+  repo_url="https://github.com/dsw56021/GitMerging.git"
+  target_dir=$(pwd)
+
+  echo "Klonowanie repozytorium do katalogu: $target_dir..."
+  git clone "$repo_url" "$target_dir"
+
+  if [ $? -eq 0 ]; then
+    echo "Repozytorium zostało pomyślnie sklonowane."
+
+
+    echo "Dodawanie katalogu do zmiennej PATH..."
+    export PATH="$target_dir:$PATH"
+    echo "Aktualna wartość zmiennej PATH: $PATH"
+  else
+    echo "Wystąpił problem podczas klonowania repozytorium."
+  fi
 elif [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
   echo "Aktualna data: $(date)"
 elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
